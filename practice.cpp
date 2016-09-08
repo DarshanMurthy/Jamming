8e3e4d49c06bb15bd1196ec257399837d2fd88ce
DarshanMurthy <darshants.darshan1@gmail.com>
2016-09-07 18:54:31 -0700

    code

--- practice.cpp
+++ practice.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 
+struct node{
+	int name;
+	char natrur[2] ={'T','F'};
+};
+
 
 int main(){
 
+	node Node;
+	cout<<Node.natrur[1]<<endl;
+
+
+	int value[12][12];
+
+	for(int i=0 ;i<12;i++){
+		for(int j=0;j<12;j++){
+			value[i][j] =i*j;
+		}
+	}
+
+	for(int i=0;i<12;i++){
+		for(int j=0;j<12;j++){
+			cout<< value[i][j]<<endl;
+		}
+	}
 
 
 
