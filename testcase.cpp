f1283c2297a877c4007f8454bd27c3ad921e7233
DarshanMurthy <darshants.darshan1@gmail.com>
2016-11-01 08:51:22 -0700

    add plus

---
+++ testcase.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+
+
+
+using namespace std;
+int main(){
+
+	double memo[1<<16];
+	int  data = 16;
+	int test = data<<2;
+	cout<<test;
+
+
+
+	return 0;
+
+}
