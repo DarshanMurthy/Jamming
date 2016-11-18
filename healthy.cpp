0cb47d468c1d13ad252598317d0b2d98f4e9dc86
dtumkursreenivas <dtumkursreenivas@apple.com>
2016-11-17 17:49:26 -0800

    temp

--- healthy.cpp
+++ healthy.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
+#include<stdlib.h>
+#include<vector>
+
+using namespace std;
 struct Node{
 	int data;
 	struct Node * llink ;
 	struct Node * rlink;
+};
+
+
+int main(){
+	double value=10;
+
+	cout<< sizeof(Node)<<endl;
+
+
+
+	struct Node * ptr;
+	ptr = (Node *) malloc(sizeof(Node));
+	ptr-> data=100;
+	cout<< ptr->data;
+
+	vector<int> vtr(100);
+	cout<< vtr.size();
+	for(int i=0;i<100;i++){
+		vtr.push_back(i);
+		}
+
+
+	for(char i='a';i<'z';i++){
+		cout<< i <<endl;
+	}
+
+
+
+	
+
 }
