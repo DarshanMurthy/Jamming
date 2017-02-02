d6d8f3d4dc76d977c1236598a374dff771d0a098
dtumkursreenivas <dtumkursreenivas@apple.com>
2017-02-01 17:58:43 -0800

    added the cpp files

--- practice.cpp
+++ practice.cpp
@@ -1,53 +1,31 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <cstdlib>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-
-#define _CRT_SECURE_NO_DEPRECATE
-
-struct Node{
-	int data;
-	struct Node * llink;
-
-};
+#include<iostream>
 using namespace std;
 
+struct Node{
+	int add;
+};
+
 int main(){
+	int array[10][10]={{10,24,12},{10,29,34}};
+	cout<< sizeof(array[0]);
 
-	FILE * fin;
+	cout<< sizeof(array)<<endl;
 
+	int len;
+	len = sizeof(array)/sizeof(array[0]);
+	cout<<len;
 
-
-	int arr[3][3] ={{1,12,3},{9,8,2},{89,87,12}};
-
-
-	for(int i=0;i<3;i++){
-
-		for(int j=0;j<3;j++){
-
-			cout<< arr[i][j] ;
-			cout<< " ";
-			
-
-
-		}
-
-		cout<<endl;
-	}
+	struct Node * llink;
+	stuct Node * rrlink;
 
 
 
+
+
+
+
+
+
+
+	return 0;
 }
