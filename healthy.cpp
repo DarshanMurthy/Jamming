4b331c97e9af93cfbeddf7b5f0b976a1fd81258c
dtumkursreenivas <dtumkursreenivas@apple.com>
2017-02-16 06:02:46 -0800

    added the code to problem

--- healthy.cpp
+++ healthy.cpp
@@ -2,22 +2,35 @@
 
 using namespace std;
 
-void permutation(char * str, int start, int end){
-  cout<< str;
+struct Node{
+  int data;
+  struct Node * llink;
+  struct Node * rlink;
+};
+
+void swap(char * t1, char * t2){
+  char * temp;
+  temp = t1;
+  t1= t2;
+  t2 = temp;
   
 
 }
 
+void permute(char * a, int start, int end){
+  for(int i=start;i<end;i++){
+    cout<< a+start<<endl;
+    cout<< a+i<<endl;
+    
+    swap(a+start,a+i);
 
+  }
+}
 
 int main(){
-  char namePtr []= "darshan tumkur";
-  int len = strlen(namePtr);
-  permutation(namePtr,0,len-1);
-
+  Node * node;
+  char name [] = "darshan";
+  int len = strlen(name);
+  permute(name, 0, len-1);
   return 0;
-
-
-
-
 }
--- practice.cpp
+++ practice.cpp
@@ -1,30 +1,49 @@
 #include<iostream>
 using namespace std;
+void permute(char * str, int first, int last);
 
 struct Node{
-	int add;
-	struct Node * llink;
-	struct Node * rrlink;
-};
-
-struct NodeTest{
-  int add;
   int data;
   struct Node * llink;
   struct Node * rrlink;
+};
+
+void swap(char * str1, char * str2)
+{
+  char  * temp;
+  temp = str1;
+  str1 = str2;
+  str2 = temp;
+}
+
+
+void permute(char * str, int first, int last){
+
+  if(first == last){
+    cout<< str;
+
+  }
+  else {
+  for(int first=0;first<last-1;first++){
+
+    swap(str+first, str+last);
+    permute(str, str+first, str+last);
   
-};
 
-struct TestNodeTest{
-  int data;
-};
-
+  }
+  }
+}
 
 
 int main(){
-        int test[10][10];
-	int array[10][10]={{10,24,12},{10,29,34}};
-	struct NodeTest * llink = malloc(sizeof(NodeTest));
-	
-	return 0;
+  char name[] = "darshan";
+  cout<< name;
+
+  int len = strlen(name);
+  permute(name,0,len-1);
+
+
+  return 0;
+
 }
+
