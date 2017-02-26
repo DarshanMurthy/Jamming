216c2bae4544b2bd9fffa3001c3411864bc312ce
dtumkursreenivas <dtumkursreenivas@apple.com>
2017-02-26 08:23:44 -0800

    added the code

--- helptestcase.cpp
+++ helptestcase.cpp
@@ -1,15 +1,22 @@
 #include<iostream>
+struct Node{
+  int data;
+  struct Node * llink;
+  struct Node * rlink;
+};
+
 using namespace std;
 
 int main(){
+  Node * first = (Node *) malloc(sizeof(Node));
+  first->data=100;
+  first->llink='\0';
   
-  char * name="darshan100";
-  char temp[]="darlen";
+  char * name="darshanmurthy";
+  cout<< strlen(name)<<endl;
+  cout<< sizeof(name)<<endl;
 
-  
-  cout<< name<<endl;
-  
-
-  return 0;
-
+  char name1[]="darshanmurthy";
+  cout<< sizeof(name1) << endl;
+  cout<< strlen(name1) <<endl;
 }
